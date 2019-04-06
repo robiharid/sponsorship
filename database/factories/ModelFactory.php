6620748e690be1b4ab231324409dcce4c97dbe44
Robi H <robiharid@gmail.com>
2019-04-06 15:10:56 +0100

    Test viewing the sponsorship page complete

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,7 +13,7 @@ use Faker\Generator as Faker;
 | your application. Factories provide a convenient way to generate new
 | model instances for testing / seeding your application's database.
 |
-*/
+ */
 
 $factory->define(User::class, function (Faker $faker) {
     return [
@@ -23,4 +23,11 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
+});
+
+$factory->define(App\Sponsorable::class, function (Faker $faker) {
+    return [];
+});
+$factory->define(App\SponsorableSlot::class, function (Faker $faker) {
+    return [];
 });
